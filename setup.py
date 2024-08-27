9158b37c3ce750b79954a3ae042857046d534671
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-08-27 17:51:47 +0530

    file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name="mcqgenrator",
+    version="0.0.1",
+    author="Satyam Singh",
+    author_email="satyamsinghss2203@gmail.com",
+     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+     packages= find_packages()
+)
